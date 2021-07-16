7a37e14ed0a7bec2133c2107e5bac1c4b06feb2d
Ri Le <rl2999@columbia.edu>
2021-07-16 17:24:30 -0400

    auto imports

--- models/index.js
+++ models/index.js
@@ -5,29 +5,43 @@ const { Sequelize, DataTypes } = require("sequelize");
 
 // Sequelize session
 console.debug("Starting database connection: ", CONFIG.DB.CONNECTION);
-const sequelize = new Sequelize({
+const session = new Sequelize({
   dialect: "sqlite",
   storage: CONFIG.DB.CONNECTION,
 });
 
-// Setup all models
-const Annotation = require("./Annotation")(sequelize, DataTypes);
-const Collection = require("./Collection")(sequelize, DataTypes);
-const ItemList = require("./ItemList")(sequelize, DataTypes);
-const MetItem = require("./MetItem")(sequelize, DataTypes);
-const User = require("./User")(sequelize, DataTypes);
+// Setup models
+const setupModel = function (module) {
+  const theModule = require(`./${module}`)(session, DataTypes);
+  AllModels.push(theModule);
+  return theModule;
+};
+
+// Use all the models!
+const fs = require("fs");
+const AllModels = [];
+
+fs.readdir("./models", (err, files) => {
+  files.forEach((file) => {
+    if (file.includes(".DS_Store") || file.includes("index")) {
+      return;
+    }
+    console.debug("Using model:", file);
+    setupModel(file);
+  });
+});
 
 // Create a DB if it doesn't exist
 const initializeDb = async function () {
   console.debug("Synchronizing all models...");
-  await sequelize.sync({ force: false });
+  await session.sync({ force: false });
   console.log("All models were synchronized successfully.");
 };
 
 const testConnection = async function () {
   try {
     console.debug("Testing database connection...");
-    await sequelize.authenticate();
+    await session.authenticate();
     console.debug("Connection has been established successfully!");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
@@ -44,11 +58,12 @@ Startup();
 
 module.exports = {
   testConnection,
-  sequelize: Sequelize,
+  sequelize: session,
   // Models? maybe should go somewhere else
-  Annotation,
-  Collection,
-  User,
-  ItemList,
-  MetItem,
+  ...AllModels,
+  // Annotation,
+  // Collection,
+  // User,
+  // ItemList,
+  // MetItem,
 };
